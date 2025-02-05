8ee1f0a8e7ae178078badcd8413dd5b14bf22ae1
hans.arias <hansarias74@gmail.com>
2025-02-05 00:59:19 -0500

    first commit, mono-repo, using git

---
+++ web-soccer-bet/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div>
+        <h1 class="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Welcome to Soccer BET 
+        </h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ web-soccer-bet/pages/tournaments/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Tournament {{ id }} </h1>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params;
+
+    definePageMeta({
+        layout: 'tournaments'
+    })
+</script>
+
+<style scoped>
+
+</style>
